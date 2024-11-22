eff1a289591065e60b93020eff3006834a4f9130
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-22 09:47:22 +0000

    game table created

--- animalapp/src/components/AnimalTable.js
+++ animalapp/src/components/AnimalTable.js
@@ -24,5 +24,39 @@ export default function AnimalTable(props){
         }
     }
 
-    return();
+    return(
+        <div className="gameContainer">
+            <table className="game_table">
+                <thead>
+                    <tr>
+                        <th colSpan={3}><h1>ANIMAL MATCHING GAME</h1></th>
+                    </tr>
+                    <tr>
+                        <th><h2>Result</h2></th>
+                        <th><h2>Animal Name</h2></th>
+                        <th><h2>Select the Animal</h2></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><h3>{result}</h3></td>
+                        <td><h3>{randomAnimal.name.toUpperCase()}</h3></td>
+                        <td>
+                            <div>
+                                <div>
+                                    {
+                                        animals.map((animal)=>(
+                                         <div key={animal.name} onClick={()=>handleAnimalClick(animal.name)}>
+                                            <img src={require('../assests/img/$(animal.name)')} alt= {animal.name}/>
+                                         </div>   
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
