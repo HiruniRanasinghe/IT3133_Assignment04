456f92ab67e4acffe0d6681b66e68b2657148b0d
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-22 08:41:10 +0000

    first function created

--- animalapp/src/components/AnimalTable.js
+++ animalapp/src/components/AnimalTable.js
@@ -3,8 +3,17 @@ import { animals } from '../assests/data/AnimalsDb';
 import '../assests/css/compo.csss';
 
 export default function AnimalTable(props){
-    
-    return(
+    const [randomAnimal,setRandomAnimal] = useState(null);
+    const [result,setResult] = useState('');
+    useEffect(()=>{
+        generateRandomAnimal();
+    },[]);
 
-    );
+    const generateRandomAnimal = () =>{
+        randomIndex = Math.floor(Math.random() * 10) + 1;
+        setRandomAnimal(animals[randomIndex]);
+        setResult('');
+    }
+
+    return();
 }
