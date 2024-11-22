a9b486053b73ac71e6eebdb5b4cbfb92af66e7d6
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-22 08:17:13 +0000

    Base layout is created in app

---
+++ animalapp/src/components/AnimalTable.js
@@ -0,0 +1,10 @@
+import React,{ useState,useEffect } from 'react';
+import { animals } from '../assests/data/AnimalsDb';
+import '../assests/css/compo.csss';
+
+export default function AnimalTable(props){
+    
+    return(
+
+    );
+}
